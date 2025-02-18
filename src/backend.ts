db5bb5b42ddcd5926a29532d2dee0660a11e3d99
Edouard-Hashimoto <edouard.hashimoto@edu.univ-fcomte.fr>
2025-02-18 13:50:21 +0000

    Changement

--- src/backend.ts
+++ src/backend.ts
@@ -1,4 +1,4 @@
 import Pocketbase from "pocketbase";
 import type { TypedPocketBase } from "./pocketbase-typegen";
 
-export const pb = new Pocketbase("http://185.216.25.92:8190/") as TypedPocketBase;
+export const pb = new Pocketbase("http://0.0.0.0:8190/") as TypedPocketBase;
